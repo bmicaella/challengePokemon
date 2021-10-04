d6c497760a845c2524f04d6414c86e919fc8179a
Micaella Barcellos Pereira <micaellab30@gmail.com>
2021-10-04 20:06:28 -0300

    att home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,15 +4,16 @@ import logo from '../../assets/img/logo.png'
 import './style.scss';
 
 function Home() {
-    // const history = useHistory();
-    // const navigateTo = () => history.push('/game');
+    const history = useHistory();
+    const navigateTo = () => history.push('/game');
 
     return (
         <main>
             <section id="sct-home"> 
                 <img src={logo} alt="logo"/>
-                {/* <button type="button" onClick={navigateTo}>COMEÇAR</button> */}
-                <button type="button" onClick={()=> window.location.href='/game'}>COMEÇAR</button>
+                <button type="button" onClick={navigateTo}>COMEÇAR</button>
+                {/* <button type="button" onClick={()=> window.location.href='/game'}>COMEÇAR</button> */}
+                {/* <button type="button" onClick={()=> history.push("/game", { from: "Home" })}>COMEÇAR</button> */}
             </section>
         </main>
   );
